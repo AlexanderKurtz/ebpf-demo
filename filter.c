d00c9ce764a0017a1d9e0d417c33513972fd17f8
Alexander Kurtz <alexander@kurtz.be>
2017-10-11 16:50:41 +0200

    filter.c: Fix an incorrect comment.

--- filter.c
+++ filter.c
@@ -2,10 +2,7 @@
 #include <bcc/proto.h>
 
 int filter(struct __sk_buff *skb) {
-	/* We are testing this over the loopback interface which uses raw UDP
-	   packets without encapsulating them into Ethernet frames or IP
-	   packets, so the offset of the UDP length field is simply 4.
-	 */
+	// The UDP packet starts at offset 0, so the length is at offset 4.
 	unsigned short length = load_half (skb, 4);
 
 	if (length == 8) {
